30e13db58b19551eb767e9667f33cb0d4af1ea46
Thanh Tùng Ngô <tungnt@i.lisod.vn>
2024-03-12 14:27:12 +0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import EditorComponent from "./components/EditorComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <EditorComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/EditorComponent.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useRef } from "react";
+
+const EditorComponent = () => {
+  return (
+    <>
+      <div className="h-5" id="editorjs"></div>
+    </>
+  );
+};
+
+export default EditorComponent;
