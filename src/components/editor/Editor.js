ee3791313678365cd8b793e5f6635dee947b3ff3
Noirah <tungnt@i.lisod.vn>
2024-05-08 14:26:57 +0700

    inline drag and drop

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -6,20 +6,27 @@ import { EDITOR_JS_TOOLS } from "./tools/tools";
 // create editor instance
 import { createReactEditorJS } from "react-editor-js";
 import { ColorPicker, InputNumber } from "antd";
+import DragDrop from "editorjs-drag-drop";
 
 export default function Editor({ data, setData, setTitleColor }) {
   const editorCore = useRef(null);
   const ReactEditorJS = createReactEditorJS();
   const [textColor, setTextColor] = useState("#000000");
 
-  const handleInitialize = useCallback((instance) => {
-    // await instance._editorJS.isReady;
-    instance._editorJS.isReady
-      .then(() => {
-        // set reference to editor
-        editorCore.current = instance;
-      })
-      .catch((err) => console.log("An error occured", err));
+  const instanceRef = useRef(null);
+
+  // const handleInitialize = useCallback((instance) => {
+  //   // await instance._editorJS.isReady;
+  //   instance._editorJS.isReady
+  //     .then(() => {
+  //       // set reference to editor
+  //       editorCore.current = instance;
+  //     })
+  //     .catch((err) => console.log("An error occured", err));
+  // }, []);
+
+  const handleInitialize = React.useCallback((instance) => {
+    editorCore.current = instance;
   }, []);
 
   const handleSave = useCallback(async () => {
@@ -29,6 +36,11 @@ export default function Editor({ data, setData, setTitleColor }) {
     // setData(savedData);
     console.log("savedData", savedData);
   }, [setData]);
+
+  const handleReady = () => {
+    const editor = editorCore.current._editorJS;
+    new DragDrop(editor);
+  };
 
   return (
     <div className="editor-container" style={{ color: textColor }}>
@@ -51,6 +63,8 @@ export default function Editor({ data, setData, setTitleColor }) {
       </div>
 
       <ReactEditorJS
+        instanceRef={(instance) => (instanceRef.current = instance)}
+        onReady={handleReady}
         onInitialize={handleInitialize}
         tools={EDITOR_JS_TOOLS}
         onChange={handleSave}
